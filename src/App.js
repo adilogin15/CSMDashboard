ab01f2278543599a19cdc41718c3ef9f819280fa
Aditya Sharma <adilogin@hotmail.com>
2018-08-16 15:03:01 +0530

    UI added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBox from './component/searchBox';
-import ContentBox from './component/contentBox';
-import LoginBox from './component/loginBox';
 
-let jsonData=[
-  {name: "Sporting Goods1", description: "$49.99", status: 'true', link: "Football"},
-  {name: "Sporting Goods2", description: "$9.99", status: 'true', link: "Baseball"},
-  {name: "Sporting Goods3", description: "$29.99", status: 'false', link: "Basketball"},
-  {name: "Electronics1", description: "$99.99", status: 'true', link: "iPod Touch"},
-  {name: "Electronics3", description: "$399.99", status: 'false', link: "iPhone 5"},
-  {name: "Electronics", description: "$199.99", status: 'true', link: "Nexus 7"}
-];
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        
-          <img src="http://solutionengineering.us.oracle.com/excellence/css/images/logo.png" class="App-logo" alt="logo" />
-          <span>POVaaS</span>
-        </header>
-        <LoginBox/>
-        <div id="searchPanel" class="hide">
-          <SearchBox title="" placeholder="Search" /> 
-          <ContentBox jsonData={jsonData}/>   
-          </div>      
+        <div className="sidenav">
+          <a href="#cd">CD</a>
+          <a href="#dashboard">Dashboard</a>
+        </div>
+
+        <div className="main">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+
+                <div className="card">
+                  <div className="card-content">
+                    <p className="overall">FY19 Overall usage (28%)</p>
+                    <input type="range" className="slider1" min="0" max="100" step="1" />
+                  </div>
+                </div>
+
+                <div className="card">
+                  <div className="card-content container">
+                    <p className="overallc2">Contract Type Overall usage</p>
+                    <div className="row">
+                      <div className="col-md-3 ib1">
+                        <p className="overall">Metered (28%)</p>
+                        <input type="range" className="slider2" min="0" max="100" step="1" />
+                      </div>
+                      <div className="col-md-3 ib2">
+                        <p className="overall">UC Flex Annually (28%)</p>
+                        <input type="range" className="slider2" min="0" max="100" step="1" />
+                      </div>
+                      <div className="col-md-3 ib3">
+                        <p className="overall">UC Flex Monthly (28%)</p>
+                        <input type="range" className="slider2" min="0" max="100" step="1" />
+                      </div>
+                      <div className="col-md-3 ib4">
+                        <p className="overall">UC PAYG (28%)</p>
+                        <input type="range" className="slider2" min="0" max="100" step="1" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="card">
+                  <div className="card-content container">
+                    <p className="overallc2">TBD</p>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          
+        </div>   
       </div>
     );
   }
